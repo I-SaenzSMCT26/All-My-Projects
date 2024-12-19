b40cf3f7eba12439cfab9a3308b608d3d0a31fa5
I-SaenzSMCT26 <daytradersaenz@gmail.com>
2024-12-18 20:33:36 -0600

    Updated code for the practice JS file

--- my-fun-projects/ny-javascript-practice/practice.js
+++ my-fun-projects/ny-javascript-practice/practice.js
@@ -1,7 +1,19 @@
-let country = 'United States';
-let continent = 'North America'
-let population = 334900000;
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
 
-console.log(country);
-console.log(continent);
-console.log(population);
+console.log("Isaac");
+console.log(23);
+
+let firstName = "billy";
+let first = 'bob';
+let first_name = 'Ray';
+let last_name = 'Saenz';
+
+console.log(first_name);
+console.log(last_name);
+console.log(firstName);
+
+let new_name = 'Rob';
+console.log(new_name);
+
+// Dollars signs $ and underscores _ are the only symbols that are allowed in variable names such as $first_name.
