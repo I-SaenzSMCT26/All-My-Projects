072c4e8a565c7e83960073210ff9146a361f647d
I-SaenzSMCT26 <daytradersaenz@gmail.com>
2024-12-19 22:46:36 -0600

    updated: 12/19/24

--- ny-javascript-practice/practice.js
+++ ny-javascript-practice/practice.js
@@ -1,46 +1,61 @@
-// Dollars signs $ and underscores _ are the only symbols that are allowed in variable names such as $first_name.
+// // Dollars signs $ and underscores _ are the only symbols that are allowed in variable names such as $first_name.
 
-// a variable is a box that we can store a value in.
+// // a variable is a box that we can store a value in.
 
-// convention variables are all caps.
+// // convention variables are all caps.
 
-// always make sure that all your code is written in a clean manner. always!!
-
-
-
-// these two lines below are much easier to understand than the two lower ones
-let myFirstJob = 'Programmer';
-let myCurrentJob = 'Teacher';
-
-// these two variables are a lot harder to understand the meaning of than the two on top.
-let job1 = 'Programmer';
-let job2 = 'Teacher';
-
-console.log(myCurrentJob);
-console.log(myFirstJob);
-console.log(job1);
-console.log(job2);
+// // always make sure that all your code is written in a clean manner. always!!
 
 
-let language = 'English';
 
-// number: floating point numbers - used for decimals and integers.
-let age = 23;
+// // these two lines below are much easier to understand than the two lower ones
+// let myFirstJob = 'Programmer';
+// let myCurrentJob = 'Teacher';
 
-// string: sequence of characters - used for text.
-let first_Name = 'George';
+// // these two variables are a lot harder to understand the meaning of than the two on top.
+// let job1 = 'Programmer';
+// let job2 = 'Teacher';
 
-// boolean: logical type that can only be true or false.
-let fullAge = true;
-
-
-// undefined: value taken by a variable that is not yet defined('empty value').
-// Null: also means 'empty value'.
-let children;
-
-// symbol: value that is unique and connot be changed [not useful for now]
-
-// BigInt: larger integers than the number type can hold.
+// console.log(myCurrentJob);
+// console.log(myFirstJob);
+// console.log(job1);
+// console.log(job2);
 
 
-// JavaScript has dynamic typing: we do not have to manually define the data type of the value stored in a variable. Instead, data types are determined automatically.
+// let language = 'English';
+
+// // number: floating point numbers - used for decimals and integers.
+// let age = 23;
+
+// // string: sequence of characters - used for text.
+// let first_Name = 'George';
+
+// // boolean: logical type that can only be true or false.
+// let fullAge = true;
+
+
+// // undefined: value taken by a variable that is not yet defined('empty value').
+// // Null: also means 'empty value'.
+// let children;
+
+// // symbol: value that is unique and connot be changed [not useful for now]
+
+// // BigInt: larger integers than the number type can hold.
+
+
+// // JavaScript has dynamic typing: we do not have to manually define the data type of the value stored in a variable. Instead, data types are determined automatically.
+
+
+// let age = 30;
+// age = 32;
+
+// const firstName = 'Isaac'; /*the const function is basically what it sounds like. it is constant, which means 
+// that you can not change the value of the variable in the future. it is immutable.*/
+
+// let favoriteFood = 'Pizza'; //the 'let' function allows you to change the value of a variable in the future if you choose to do so.
+
+// var last_Name = 'Keinspinski'; // never use var to define a variable. this basically an obsolete method of defining a variable.
+
+const birthyearIsaac = 2000;
+const birthyearSean = 2006;
+console.log(birthyearIsaac);
