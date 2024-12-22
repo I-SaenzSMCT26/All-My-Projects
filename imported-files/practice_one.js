fe7cf2b2f917054308db2b94db325d0284219c4c
I-SaenzSMCT26 <daytradersaenz@gmail.com>
2024-12-22 15:09:31 -0600

    updated: 12/22/24 3:09pm

--- imported-files/practice_one.js
+++ imported-files/practice_one.js
@@ -1,37 +1,50 @@
-const now = 2037;
+// const now = 2037;
 
-const ageJonas = now - 1991;
-const ageIsaac = now - 2018;
-const ageHoward = now - 2019;
-console.log(ageJonas, ageIsaac);
+// const ageJonas = now - 1991;
+// const ageIsaac = now - 2018;
+// const ageHoward = now - 2019;
+// console.log(ageJonas, ageIsaac);
 
-console.log(ageJonas * 2, ageIsaac / 2, 2**3);
-//2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+// console.log(ageJonas * 2, ageIsaac / 2, 2**3);
+// //2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
 
-const firstName = 'Isaac';
-const lastName = 'Saenz';
-console.log(firstName + ' ' + lastName);
+// const firstName = 'Isaac';
+// const lastName = 'Saenz';
+// console.log(firstName + ' ' + lastName);
 
-let x = 10 + 5; //15
-x += 10; //x = x + 10 = 25
-x *= 4; // x = x * 4 = 100
-x++; // x = x + 1
-x--;
-console.log(x);
+// let x = 10 + 5; //15
+// x += 10; //x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1
+// x--;
+// console.log(x);
 
-//Comparison operators
-console.log(ageJonas > ageIsaac);
-console.log(ageIsaac >= 18);
+// //Comparison operators
+// console.log(ageJonas > ageIsaac);
+// console.log(ageIsaac >= 18);
 
-const isFullAge = ageIsaac >= 18;
+// const isFullAge = ageIsaac >= 18;
 
-console.log(now - 1991 > now - 2018);
+// console.log(now - 1991 > now - 2018);
 
-console.log(25 - 10 - 5);
+// console.log(25 - 10 - 5);
 
-let t, y;
-t = y = 25 - 10  - 5;
-console.log(x, y); 
+// let t, y;
+// t = y = 25 - 10  - 5;
+// console.log(x, y); 
 
-const averageAge = (ageIsaac + ageJonas) / 2;
-console.log(ageIsaac, ageJonas, averageAge)
+// const averageAge = (ageIsaac + ageJonas) / 2;
+// console.log(ageIsaac, ageJonas, averageAge)
+
+const marksMass = 78;
+const johnsMass = 92;
+const marksHeight = 1.69 + 'm';
+const johnsHeight = 1.69 + 'm';
+
+const markssecondMass = 95 + 'kg';
+const johnssecondMass = 85 + 'kg';
+const markssecondHeight = 1.88 + 'm';
+const johnssecondheight = 1.76 + 'm';
+
+const mass = (marksMass + johnsMass) + 'kg';
+console.log(mass)
