e0a4add381f4e8d762d4703de393d0af87686b53
saenz-smct-26 <saenz.smct.26@gmail.com>
2024-12-31 19:42:33 -0600

    new update

--- imported-files/tip-calculator.js
+++ imported-files/tip-calculator.js
@@ -1,12 +1,13 @@
-const bill = Number(prompt()); /* The prompt has to be inside of the number 
+const bill = Number(prompt(`Enter Price: `)); /* The prompt has to be inside of the number 
 declaration because by default the prompt produces a string. This forces the prompt to produce a number value */
+
 
 if(bill <= 9){
     console.log(`Tip not worth it; don't bother leaving one 😅... 
 or just leave one I dont care.🤷‍♂️`);
 } else{
     const tip = 
-    bill > 1500 && bill <= 4000 ? bill * .39 :
+    bill > 1500 && bill <= 4000 ? bill * .4 :
     bill > 600 && bill <= 1500 ? bill * .28 : 
     bill > 300 && bill <= 600 ? bill * .2  : 
     bill > 50 && bill <= 300 ? bill * .15 : 
