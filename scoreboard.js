e5a91c831df98ebdda944ca5e551b3f379f87b57
saenz-smct-26 <saenz.smct.26@gmail.com>
2025-01-03 23:33:01 -0600

    new files and updated files

--- scoreboard.js
+++ scoreboard.js
@@ -1,5 +1,33 @@
-const scoreBoard = document.getElementById("score-board");
+let home = 0;
+let opponent = 0;
 
-function addScore(teamScores){
-    scoreBoard.value += teamScores;
+let scoreHome = document.getElementById('home');
+let scoreOpp = document.getElementById('opponent');
+
+
+let plusButton = (teamPoints) => {
+    if (teamPoints === 'teamHome'){
+        home += 5;
+        scoreHome.textContent = home;
+    } else if(teamPoints === 'teamOpp'){
+        opponent += 5;
+        scoreOpp.textContent = opponent;
+    }
+};
+
+let minusButton = (teamPoints) => {
+    if(teamPoints === 'teamHome' && home > 0){
+        home -= 5;
+        scoreHome.textContent = home;
+    } else if(teamPoints === 'teamOpp' && opponent > 0){
+        opponent -= 5;
+        scoreOpp.textContent = opponent;
+    }
+};
+
+let resetBtn = () => {
+    home = 0;
+    opponent = 0;
+    scoreHome.textContent = home;
+    scoreOpp.textContent = opponent;
 }
