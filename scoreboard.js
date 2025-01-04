4289952b97ff1b49c4764d2dd2e8258323a1b5ac
saenz-smct-26 <saenz.smct.26@gmail.com>
2025-01-04 13:57:29 -0600

    updated: scoreboard

--- scoreboard.js
+++ scoreboard.js
@@ -1,33 +1,48 @@
+let scoreScreenHome = document.getElementById('home');
+let scoreScreenOpp = document.getElementById('opponent');
+
+
 let home = 0;
 let opponent = 0;
-
-let scoreHome = document.getElementById('home');
-let scoreOpp = document.getElementById('opponent');
-
 
 let plusButton = (teamPoints) => {
     if (teamPoints === 'teamHome'){
         home += 5;
-        scoreHome.textContent = home;
+        scoreScreenHome.textContent = home;
     } else if(teamPoints === 'teamOpp'){
         opponent += 5;
-        scoreOpp.textContent = opponent;
+        scoreScreenOpp.textContent = opponent;
     }
 };
 
 let minusButton = (teamPoints) => {
     if(teamPoints === 'teamHome' && home > 0){
         home -= 5;
-        scoreHome.textContent = home;
+        scoreScreenHome.textContent = home;
     } else if(teamPoints === 'teamOpp' && opponent > 0){
         opponent -= 5;
-        scoreOpp.textContent = opponent;
+        scoreScreenOpp.textContent = opponent;
     }
 };
 
 let resetBtn = () => {
     home = 0;
     opponent = 0;
-    scoreHome.textContent = home;
-    scoreOpp.textContent = opponent;
+    scoreScreenHome.textContent = home;
+    scoreScreenOpp.textContent = opponent;
 }
+
+
+//media queries section
+function updatedTeamNames(){
+    
+    const oppSideName = document.getElementById('opponentName');
+    
+    const mediaQuery = window.matchMedia(`(max-width: 878px)`);
+    
+    mediaQuery.matches ? oppSideName.textContent = "Opp" : oppSideName.textContent = "Opponent";
+}
+
+updatedTeamNames();
+window.addEventListener("resize", updatedTeamNames);
+
