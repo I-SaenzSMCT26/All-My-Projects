55f4d281cd6bc944fd73cb75b5225533d2344165
saenz-smct-26 <saenz.smct.26@gmail.com>
2025-02-01 01:06:58 -0600

    guess game

--- first course prjcts/guess.js
+++ first course prjcts/guess.js
@@ -18,17 +18,18 @@ wrongNumber1.src = 'wrong number sounds/wrong-number-1.mp3';
 let wrongNumber2 = new Audio();
 wrongNumber2.src = 'wrong number sounds/wrong-number-2.mp3';
 let wrongNumber3 = new Audio();
-wrongNumber3.src = 'wrong number sounds/wrong-number-3.mp3'
+wrongNumber3.src = 'wrong number sounds/wrong-number-3.mp3';
 
-let playOnceOnly = false
+let playOnceOnly = false;
+let addPointsOnce = false;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
     
-    if (!guess) {
-      document.querySelector('.message').textContent = 'No Number!';
-    } 
+if (!guess) {
+  document.querySelector('.message').textContent = 'No Number!';
+} 
   
 else if (guess !== secretNumber) {
 
@@ -38,52 +39,47 @@ else if (guess !== secretNumber) {
   (score -= 5, wrongNumber2.play()) : 
   score <= 20 && score > 10 ? 
   (score -= 10, wrongNumber3.play()) : 
-  score <= 10 ? score -= 10 : null;
+  score <= 10 && score > 0 ? score -= 10 : null;
 
-  if (score !== 0 && score > 5) {
+  if (score !== 0) {
 
       document.querySelector('.message').textContent = guess > secretNumber ? 
       `Too high, try again...` : `Too low, try again...`
 
-     
-
-      
       document.querySelector('.score').textContent = score;
-  } else {
+  } else{
       if(!playOnceOnly){
         youLose.play();
         playOnceOnly = true; 
       }
 
-      score === 0 ? highScore = score : null;
-
       document.querySelector('.message').textContent = `Sorry you lose. :(...`;
-      document.querySelector('.score').textContent = 0;
+      document.querySelector('.score').textContent = score;
+
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
 
       document.querySelector('body').style.backgroundColor = '#e70808';
   }
+
+} else if(guess === secretNumber) {
+  if(!playOnceOnly){
+    youWin.play();
+    playOnceOnly = true;
+  }
+
+  document.querySelector('.message').textContent = 'Nice, you guessed it!!';
+  document.querySelector('.number').textContent = secretNumber;
+
+  document.querySelector('body').style.backgroundColor = '#5ac8f3';
+  document.querySelector('.number').style.width = '30rem';
+
+  if (!addPointsOnce){
+    document.querySelector('.highscore').textContent = highScore+=120;
+    addPointsOnce = true;
+  }
+
 }
-  
-  
-                        
-    else if(guess === secretNumber) {
-      if(!playOnceOnly){
-        youWin.play();
-        playOnceOnly = true;
-      }
-   
-      document.querySelector('.message').textContent = 'Nice, you guessed it!!';
-      document.querySelector('.number').textContent = secretNumber;
-
-      document.querySelector('body').style.backgroundColor = '#5ac8f3';
-      document.querySelector('.number').style.width = '30rem';
-
-        if(guess === secretNumber){
-          document.querySelector('.highscore').textContent = highScore+=120;
-        } else {
-          document.querySelector('.highscore').textContent = null;
-        }        
-    }
                     
     
 
@@ -92,6 +88,8 @@ else if (guess !== secretNumber) {
 document.querySelector('#again').addEventListener('click', function(){
   againButton.play();
   playOnceOnly = false
+  addPointsOnce = false;
+
 
   secretNumber = Math.trunc(Math.random()*50) + 1;
   score = 50;
--- my-fun-projects/scoreboard/scoreboard.js
+++ my-fun-projects/scoreboard/scoreboard.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let scoreScreenTeam = document.getElementById('team');
 let scoreScreenOpp = document.getElementById('opponent');
 
@@ -12,9 +14,9 @@ const minusButton = (Points) =>
     'teamTeam' === Points && team > 5 ? scoreScreenTeam.textContent = team -= 6 : 'teamOpp' === Points && opponent > 5 ? scoreScreenOpp.textContent = opponent -= 6 : 0;
 
 // Below is the code for the secondary buttons for adding points
-                const plusOneButton = (Points) => Points === 'plusOneTeam' ? scoreScreenTeam.textContent = team += 1 : Points === 'plusOneOpp' ? scoreScreenOpp.textContent = opponent += 1 : 0;
-                    const plusTwoButton = (Points) => Points === 'plusTwoTeam' ? scoreScreenTeam.textContent = team += 2 : Points === 'plusTwoOpp' ? scoreScreenOpp.textContent = opponent += 2 : 0;
-                        const plusThreeButton = (Points) => Points === 'plusThreeTeam' ? scoreScreenTeam.textContent = team += 3 : Points === 'plusThreeOpp' ? scoreScreenOpp.textContent = opponent += 3 : 0;
+    const plusOneButton = (Points) => Points === 'plusOneTeam' ? scoreScreenTeam.textContent = team += 1 : Points === 'plusOneOpp' ? scoreScreenOpp.textContent = opponent += 1 : 0;
+        const plusTwoButton = (Points) => Points === 'plusTwoTeam' ? scoreScreenTeam.textContent = team += 2 : Points === 'plusTwoOpp' ? scoreScreenOpp.textContent = opponent += 2 : 0;
+            const plusThreeButton = (Points) => Points === 'plusThreeTeam' ? scoreScreenTeam.textContent = team += 3 : Points === 'plusThreeOpp' ? scoreScreenOpp.textContent = opponent += 3 : 0;
 
 
 let resetBtn = () => {
