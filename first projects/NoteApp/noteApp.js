17eac42ec03150ae8d48f772e05bb2f217bed1cb
saenz-smct-26 <saenz.smct.26@gmail.com>
2025-02-04 17:13:12 -0600

    new save

---
+++ first projects/NoteApp/noteApp.js
@@ -0,0 +1,9 @@
+let titleInput = document.querySelector('.titleInput');
+let addNote = document.querySelector('#plus');
+let noteTitle =  document.querySelector('.title');
+
+addNote.addEventListener('click', () => {
+    noteTitle.textContent = 
+})
+
+  
