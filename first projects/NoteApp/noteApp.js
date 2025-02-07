2b08307ef83dfd9a89241f1889244557679815a7
saenz-smct-26 <saenz.smct.26@gmail.com>
2025-02-06 20:31:55 -0600

    new saves

--- first projects/NoteApp/noteApp.js
+++ first projects/NoteApp/noteApp.js
@@ -1,9 +1,36 @@
-const titleInput = document.querySelector('.titleInput');
-const addNote = document.querySelector('#plus');
-const noteTitle =  document.querySelector('.title');
+const titleInput = document.querySelector(".titleInput");
+const plus = document.querySelector("#plus");
+const main = document.querySelector("main");
 
-addNote.addEventListener('click', () => {
-    noteTitle.textContent = titleInput.value;
+plus.addEventListener("click", () => {
+  const noteCard = document.createElement(`div`);
+  noteCard.classList.add("note");
+
+  const noteTitle = document.createElement(`p`);
+  noteTitle.classList.add(`title`);
+  noteTitle.textContent = titleInput.value;
+
+  const hRule = document.createElement(`hr`);
+
+  const contentOfCard = document.createElement(`p`);
+  contentOfCard.classList.add(`noteContent`);
+
+  const span = document.createElement(`span`);
+  span.contentEditable = true;
+  contentOfCard.appendChild(span);
+
+  const closeBtnContainer = document.createElement(`div`);
+  closeBtnContainer.classList.add(`closeBtnContainer`);
+
+  const closeBtn = document.createElement(`button`);
+  closeBtn.classList.add(`delContBtn`);
+  closeBtn.textContent = `✖`;
+  closeBtnContainer.appendChild(closeBtn);
+
+  noteCard.appendChild(noteTitle);
+  noteCard.appendChild(hRule);
+  noteCard.appendChild(contentOfCard);
+  noteCard.appendChild(closeBtn);
+
+  main.appendChild(noteCard);
 });
-
-  
